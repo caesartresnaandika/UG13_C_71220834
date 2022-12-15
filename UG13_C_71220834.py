4238a31fac3e00095aca18fae7c345062cb64660
caesartresnaandika <117892797+caesartresnaandika@users.noreply.github.com>
2022-12-15 16:30:22 +0700

    Add files via upload

---
+++ UG13_C_71220834.py
@@ -0,0 +1,37 @@
+#NAMA("Tresna Andika")
+#NIM("71220834")
+import turtle
+t = turtle.Turtle()
+t.pensize(7)
+t.color('white')
+frame = turtle.Screen().bgcolor("blue")
+#C
+t.penup()
+t.goto(-200,0)
+t.pendown()
+t.right(140)
+t.circle(-50,270)
+#T
+t.penup()
+t.goto(70,70)
+t.pendown()
+t.left(50)
+t.forward(150)
+t.backward(75)
+t.right(90)
+t.forward(150)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
